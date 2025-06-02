d77d880fb73bf3d0d315faf35fc06d0dc8ffc79e
falsidge <tesetings@gmail.com>
2025-06-02 00:42:04 -0700

    1.21.1 NeoForge Port

--- src/main/java/io/github/fourmisain/perfectaccuracy/mixin/FishingBobberEntityMixin.java
+++ src/main/java/io/github/fourmisain/perfectaccuracy/mixin/FishingBobberEntityMixin.java
@@ -1,23 +1,23 @@
 package io.github.fourmisain.perfectaccuracy.mixin;
 
-import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.world.entity.projectile.FishingHook;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(FishingBobberEntity.class)
+@Mixin(FishingHook.class)
 public abstract class FishingBobberEntityMixin {
 	// >= 1.19
-	@ModifyConstant(method = "<init>(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;II)V",
-		constant = @Constant(doubleValue = 0.0103365), require = 0)
+	@ModifyConstant(method = "Lnet/minecraft/world/entity/projectile/FishingHook;<init>(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;II)V",
+			constant = @Constant(doubleValue = 0.0103365), require = 0)
 	public double modifyDeviation(double variance) {
 		return 0;
 	}
 
-	// < 1.19
-	@ModifyConstant(method = "<init>(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;II)V",
-			constant = @Constant(doubleValue = 0.0045), require = 0)
-	public double modifyOldDeviation(double variance) {
-		return 0;
-	}
+//	// < 1.19
+//	@ModifyConstant(method = "<init>(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;II)V",
+//			constant = @Constant(doubleValue = 0.0045), require = 0)
+//	public double modifyOldDeviation(double variance) {
+//		return 0;
+//	}
 }
--- src/main/java/io/github/fourmisain/perfectaccuracy/mixin/ProjectileEntityMixin.java
+++ src/main/java/io/github/fourmisain/perfectaccuracy/mixin/ProjectileEntityMixin.java
@@ -1,22 +1,22 @@
 package io.github.fourmisain.perfectaccuracy.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Projectile;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(ProjectileEntity.class)
+@Mixin(Projectile.class)
 public abstract class ProjectileEntityMixin {
 	@Shadow @Nullable
 	public abstract Entity getOwner();
 
-	@ModifyVariable(method = "setVelocity(DDDFF)V", at = @At("HEAD"), ordinal = 1, argsOnly = true)
+	@ModifyVariable(method = "Lnet/minecraft/world/entity/projectile/Projectile;shootFromRotation(Lnet/minecraft/world/entity/Entity;FFFFF)V", at = @At("HEAD"), ordinal = 4, argsOnly = true)
 	public float removeDivergenceForPlayers(float divergence) {
-		if (getOwner() instanceof PlayerEntity) return 0;
+		if (getOwner() instanceof Player) return 0;
 		return divergence;
 	}
 }
